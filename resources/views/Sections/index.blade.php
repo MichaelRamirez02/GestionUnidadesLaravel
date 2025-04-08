c94b48d97fb8834a262456a199bd845a27e3a257
MichaelRamirez <maicol.90.rami@gmail.com>
2025-04-07 22:42:13 -0500

    Sections and update of layout

---
+++ resources/views/Sections/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Secciones</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SectionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'Index']);
+Route::get('/sections', [SectionsController::class, 'Index'])->name("sections.index");
+
